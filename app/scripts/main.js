38a9b0c83e26972c36ecdcd92f2915586f132812
Maximilian Henrich <henrich.maximilian@gmail.com>
2017-08-23 20:36:18 +0200

    include bulma, add basic vue functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+/*jshint esversion: 6 */
+
+new Vue ({
+  el: "#app",
+  data: {
+    minutes: 30,
+    hours: 0
+  },
+  computed: {
+    hoursDecimal() {
+      let min = parseFloat(this.minutes) || 0;
+      let hours = (parseFloat(this.hours) * 60) || 0;
+      let minTotal = min + hours;
+      let decimale = minTotal / 60;
+      return Math.round( decimale * 100) / 100;
+    }
+  }
+
+});
